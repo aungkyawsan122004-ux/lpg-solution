313831b60d83b1e97c07cb219697234df41817db
aungkyawsan122004-ux <aungkyawsan122004@gmail.com>
2026-09-03 12:31:44 -0600

    Create TestServlet.java

---
+++ src/main/java/com/lpg/servlet/TestServlet.java
@@ -0,0 +1,20 @@
+package com.lpg.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Servlet is working successfully!</h3>");
+    }
+}
